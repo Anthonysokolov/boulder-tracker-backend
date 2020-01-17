f0a081cf7fee2afaa8ca8301f9b8ef0c5059364a
Anthony <anthony@Apples-MacBook-Pro.local>
2020-01-17 15:19:57 -0500

    updated user

--- database/models/user.js
+++ database/models/user.js
@@ -3,7 +3,11 @@ const db = require('../db')
 
 
 const User = db.define("user",{
-  name:{
+  username:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password:{
     type: Sequelize.STRING,
     allowNull: false
   },
