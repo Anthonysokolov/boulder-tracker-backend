119203c4e7965d616cec1d542ceff4d52f7c2209
Anthony <anthony@silver.local>
2020-01-21 14:17:15 -0500

    updated user model

--- database/models/user.js
+++ database/models/user.js
@@ -1,15 +1,26 @@
 const Sequelize = require('sequelize')
+const crypto = require('crypto')
 const db = require('../db')
 
 
 const User = db.define("user",{
   username:{
     type: Sequelize.STRING,
+    unique:true,
     allowNull: false
   },
   password:{
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    get(){
+      return () => this.getDataValue("password")
+    }
+  },
+  salt:{
+    type: Sequelize.STRING,
+    get(){
+      return () => this.getDataValue("salt")
+    }
   }
 })
 
